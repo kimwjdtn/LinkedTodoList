bb1bf0ec105934780c766bb2079d8277aacebb54
kimwjdtn <rhfemewjdtn@gmail.com>
2023-12-13 04:44:00 +0900

    디자인 변경, Link 앞뒤로 추가

---
+++ ToDo/View/RowView.swift
@@ -0,0 +1,18 @@
+//
+//  RowView.swift
+//  ToDo
+//
+//  Created by kimwjdtn on 12/7/23.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RowView()
+}
